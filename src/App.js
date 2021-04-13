3c48b9e6373771745819f9c203be44dd6cb477b0
José Mauricio <lmauricio-2010@hotmail.com>
2021-04-13 01:32:05 -0300

    Listar e adicionar comentário

--- src/App.js
+++ src/App.js
@@ -2,33 +2,87 @@ import react, { useState, useEffect} from 'react'
 import './App.css';
 import firebase from './firebase'
 
-const Comments = () => {
-  const ref = firebase.database().ref('test')
-  const [data, setDate] = useState({})
+const useDataBase = endpoint => {
+    const ref = firebase.database().ref(endpoint)
+    const [data, setDate] = useState({})
   
-  useEffect(() => {
-    ref.on('value', snapshot => {
-      console.log(snapshot.val())
-      setDate(snapshot.val())
-    })
-    return () => {
-      ref.off()
+    useEffect(() => {
+        ref.on('value', snapshot => {
+            console.log(snapshot.val())
+            setDate(snapshot.val())
+        })
+        return () => {
+            ref.off()
+        }
+    }, [endpoint])
+    return data
+}
+
+const useDatabasePush = endpoint => {
+    const [status, setStatus] = useState('')
+
+    const save = data => {
+        const ref = firebase.database().ref(endpoint)
+        ref.push(data, err => {
+            if(err){
+                setStatus('ERROR')
+            }else{
+                setStatus('SUCCESS')
+            }
+        })
     }
-  }, [])
-  return (
-    <pre>{JSON.stringify(data)}</pre>
-  )
+
+    return [status, save]
+}
+
+const Comment = (props) => {
+    return (
+        <div>
+            {props.comment.content} - Autor: {props.comment.user.name}
+        </div>
+    )
+}
+
+const Comments = () => {
+    const data = useDataBase('comments')
+
+    if(!data){
+        return (<pre>Nenhum comentário.</pre>)
+    }
+
+    const ids = Object.keys(data)
+
+    if(ids.length === 0){
+        return ( <p>Carregando...</p>)
+    }
+    
+    return ids.map(id => {
+        return <Comment key={id} comment={data[id]} />
+    })
 }
 
 function App() {  
-  const [visible, toggle] = useState(true)
-  
-  return (
-    <div>
-      <button onClick={() => toggle(!visible)}>Toggle</button>
-      { visible && <Comments />}
-    </div>
-  );
+    const [status, save] = useDatabasePush('comments')
+    
+    return (
+        <div>
+            <button 
+                onClick={() => {
+                    save({
+                        content:'Primeiro comentário',
+                        user: {
+                            id: '1',
+                            name: 'Mauricio'
+                        }
+                    })
+                }}
+            >Toggle</button>
+
+            Status: {status}
+
+            <Comments />
+        </div>
+    );
 }
 
 export default App;
